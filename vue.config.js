972d60ffd874af8b84e715ccbace97c0398afdc1
yanlibo2021 <yanlibo2021@126.com>
2020-04-18 18:23:20 +0800

    add src

--- vue.config.js
+++ vue.config.js
@@ -24,8 +24,9 @@ module.exports = {
    * In most cases please use '/' !!!
    * Detail: https://cli.vuejs.org/config/#publicpath
    */
-  publicPath: '/',
+  // publicPath: '/',
   // outputDir: 'dist',
+  publicPath: './',
   outputDir: 'docs',
   assetsDir: 'static',
   // lintOnSave: process.env.NODE_ENV === 'development',
